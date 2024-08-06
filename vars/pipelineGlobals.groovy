4325bc0fc93a31bbf1d303bc8dda350162e65df2
susmitha <gonepadususmithareddy917@gmail.com>
2024-08-06 12:49:26 +0530

    shared-library

---
+++ vars/pipelineGlobals.groovy
@@ -0,0 +1,9 @@
+static final String region(){
+    return "us-east-1"
+}
+static final String nexusURL(){
+    return "nexus.susmitha.online:8081"
+}
+static final String account_id(){
+    return "058264484520"
+}
